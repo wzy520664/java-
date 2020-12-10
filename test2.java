5c4630fb6fe22820b7067f01e7525c04c303fee8
wzy520664 <61545878+wzy520664@users.noreply.github.com>
2020-12-10 11:05:46 +0800

    程序3.13
    课题

---
+++ test2.java
@@ -0,0 +1,18 @@
+package test;
+import java.util.Scanner;
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner w=new Scanner(System.in);
+		float a=w.nextFloat();
+		float b=w.nextFloat();
+		float c=w.nextFloat();
+		float q;
+		double s;
+		q=(a+b+c)/2;
+		s=(float)Math.sqrt(q*(q-a)*(q-b)*(q-c));
+		System.out.println(s);
+	}
+
+}
